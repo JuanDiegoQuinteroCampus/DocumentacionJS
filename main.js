33cd28b81579c16f8558e80cdc30812df159ef96
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:53:51 -0500

    feat(docs):🚑 Strings.prototype.Split
    Borré el anterior y puse el concepto de string .split ademas de agregarle ejmeplos breves

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /* STRINGS */
 
-/* String.prototype.substr()
+/* String.prototype.split()
 
-El método substr() devuelve los caracteres de una cadena que comienzan en una localización especificada y de acuerdo al número de caracteres que se especifiquen. */
-const str = 'Mozilla';
+El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
+El método split() devuelve el nuevo array.
 
-console.log(str.substr(1, 2));
-// Expected output: "oz"
+Cuando se encuentra, el separador es eliminado de la cadena y las subcadenas obtenidas se devuelven en un array. Si el separador no es encontrado o se omite, el array contendrá un único elemento con la cadena original completa. Si el separador es una cadena vacía la cadena es convertida en un array de carácteres.
 
-console.log(str.substr(2));
-// Expected output: "zilla"
+Si el separador es una expresión regular que contiene paréntesis de captura, entonces cada vez que el separador concuerda, los resultados (incluído cualquier resultado indefinido) de los paréntesis de captura son divididos en el array resultante. Sin embargo no todos los navegadores soportan esta característica. */
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+let word = myArray[1]; 
