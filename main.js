02a0d7ce3bbbb7e6fa2df415453a8fa20905a0f2
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:56:24 -0500

    feat(docs):🚑 Strings.prototype.toLowerCase
    Borré el anterio y puse el concepto de string .toLowerCase ademas de agregarle ejmplos breves, este string pone en minuscula cierto contenido

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 /* STRINGS */
 
-/* String.prototype.split()
+/* String.prototype.toLowerCase()
 
-El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
-El método split() devuelve el nuevo array.
+El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada. toLowerCase no afecta al valor de la cadena en sí misma.
+ */
 
-Cuando se encuentra, el separador es eliminado de la cadena y las subcadenas obtenidas se devuelven en un array. Si el separador no es encontrado o se omite, el array contendrá un único elemento con la cadena original completa. Si el separador es una cadena vacía la cadena es convertida en un array de carácteres.
+let text = "Hello World!";
+let result = text.toLowerCase();
 
-Si el separador es una expresión regular que contiene paréntesis de captura, entonces cada vez que el separador concuerda, los resultados (incluído cualquier resultado indefinido) de los paréntesis de captura son divididos en el array resultante. Sin embargo no todos los navegadores soportan esta característica. */
-
-let text = "How are you doing today?";
-const myArray = text.split(" ");
-let word = myArray[1]; 
+document.getElementById("demo").innerHTML = result;
+//hello world!//
