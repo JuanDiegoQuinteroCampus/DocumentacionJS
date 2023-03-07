1b13aec3e35421fe4726c755969e3d328e9f55b6
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 13:50:52 -0500

    feat(docs):🚑 Strings.fromCharCode
    Borré el anterior y luego puse el concepto de string .fromCharCode  ademas de agregarle ejemplos

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 /* STRINGS */
 
-/* String.prototype.charCodeAt()
+/* String.fromCharCode()
+El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
+Este método devuelve una cadena y no un objeto String.
 
-El método codePointAt() devuelve un entero no negativo que equivale al valor Unicode code point del carácter. */
-'ABC'.codePointAt(1);          // 66
-'\uD800\uDC00'.codePointAt(0); // 65536
-
-'XYZ'.codePointAt(42); // undefined
+Debido a que fromCharCode es un método estático de String, usted siempre lo usará como String.fromCharCode(), en vez de un método de un objeto String creado por usted.
+ */
+String.fromCharCode(65, 66, 67);   //devuelve "ABC"
+String.fromCharCode(0x2014);       //devuelve "-"
+String.fromCharCode(0x12014);      //también devuelve "-";el dígito 1 se trunca y se ignora
+String.fromCharCode(8212);         //también devuelve "-";8212 es la forma decimal de 0x2014
