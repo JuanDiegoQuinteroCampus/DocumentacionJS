38d42b0f0f90c236236d130766d80fbadb0e08ad
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 13:45:28 -0500

    feat(docs):🚑 Strings.prototype.charCodeAt
    Borré el anterior para luego poner  el concepto de string .charCodeAt tambien le agregue varios ejemplos

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 /* STRINGS */
 
-/* String.prototype.charAt()
-ESte muestra los caracteres y sus diferentes localizaciones en la cadena */
+/* String.prototype.charCodeAt()
 
-var cualquierCadena="Brave new world";
+El método codePointAt() devuelve un entero no negativo que equivale al valor Unicode code point del carácter. */
+'ABC'.codePointAt(1);          // 66
+'\uD800\uDC00'.codePointAt(0); // 65536
 
-console.log("El carácter en el índice 0 es '" + cualquierCadena.charAt(0) + "'")
-console.log("El carácter en el índice 1 es '" + cualquierCadena.charAt(1) + "'")
-console.log("El carácter en el índice 2 es '" + cualquierCadena.charAt(2) + "'")
-console.log("El carácter en el índice 3 es '" + cualquierCadena.charAt(3) + "'")
-console.log("El carácter en el índice 4 es '" + cualquierCadena.charAt(4) + "'")
-console.log("El carácter en el índice 999 es '" + cualquierCadena.charAt(999) + "'")
-
-/* El carácter en el índice 0 es 'B'
-El carácter en el índice 1 es 'r'
-El carácter en el índice 2 es 'a'
-El carácter en el índice 3 es 'v'
-El carácter en el índice 4 es 'e'
-El carácter en el índice 999 es '' */
+'XYZ'.codePointAt(42); // undefined
