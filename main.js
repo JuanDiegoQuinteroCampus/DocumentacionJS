a2f836b0053f716cd887b42214f79ea9326b74d7
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:24:13 -0500

    feat(docs):🚑 Strings.prototype.localeCompare
    Borré el anterio y puse el concepto de string .localeCompare para luego agregarle ejemplos.

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 /* STRINGS */
 
-/* String.prototype.search()
+/* String.prototype.localeCompare()
 
-El método search() ejecuta una búsqueda que encaje entre una expresión regular y el objeto String desde el que se llama.
-Cuando se desee saber en qué casos un patrón se encuentra en una cadena de texto utiliza search() (si sólo deseas saber si existe, utiliza el método test() del prototipo de RegExp); para más información (pero de ejecución más lenta) utiliza match() (similar al método exec() de las expresiones regulares).
-El search()método compara una cadena con una expresión regular **
+El método localeCompare() retorna un número indicando si una cadena de carateres de referencia va antes, después o si es la misma que la cadena dada en orden alfabético.  */
 
-El search()método devuelve el índice (posición) de la primera coincidencia.
+const a = 'réservé'; // With accents, lowercase
+const b = 'RESERVE'; // No accents, uppercase
 
-El search()método devuelve -1 si no se encuentra ninguna coincidencia.
-
-El search()método distingue entre mayúsculas y minúsculas. */
-
-let text = "Mr. Blue has a blue house";
-let position = text.search("blue"); 
+console.log(a.localeCompare(b));
+// Expected output: 1
+console.log(a.localeCompare(b, 'en', { sensitivity: 'base' }));
+// Expected output: 0
