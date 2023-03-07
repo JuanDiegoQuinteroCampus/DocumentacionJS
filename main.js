9a18205bf9790d55402c51a8003717d5c3c348b3
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:27:03 -0500

    feat(docs):🚑 Strings.prototype.match
    Borré el contenido del commit anterior para luego ponerle el concepto de  string .match ademas de agregarle ejemplos breves

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 /* STRINGS */
 
-/* String.prototype.localeCompare()
+/* String.prototype.match()
+El método match() devuelve todas las ocurrencias de una expresión regular dentro de una cadena.
+La implementación de String.prototype.match en sí es muy simple. Se llama al método Symbol.match del argumento con la cadena como primer parámetro. La implementación real proviene de RegExp.prototype[@@match]() (en-US). */
 
-El método localeCompare() retorna un número indicando si una cadena de carateres de referencia va antes, después o si es la misma que la cadena dada en orden alfabético.  */
+const str = 'Para más información, consulte el Capítulo 3.4.5.1';
+const re = /consulte el (capítulo \d+(\.\d)*)/i;
+const found = str.match(re)
 
-const a = 'réservé'; // With accents, lowercase
-const b = 'RESERVE'; // No accents, uppercase
+console.log(found);
+// [
+//   'consulte el Capítulo 3.4.5.1',
+//   'Capítulo 3.4.5.1',
+//   '.1',
+//   index: 22,
+//   input: 'Para más información, consulte el Capítulo 3.4.5.1',
+//   groups: undefined
+// ]
+/* ----------------------------------------- */
+/* En el resultado anterior:
 
-console.log(a.localeCompare(b));
-// Expected output: 1
-console.log(a.localeCompare(b, 'en', { sensitivity: 'base' }));
-// Expected output: 0
+    'consulte el Capítulo 3.4.5.1' es la ocurrencia completa.
+    'Capítulo 3.4.5.1' fue capturado por (capítulo \d+(\.\d)*).
+    '.1' fue el último valor capturado por (\.\d).
+    La propiedad index (22) es el índice de la coincidencia completa.
+    La propiedad input es la cadena original que se analizó. */
