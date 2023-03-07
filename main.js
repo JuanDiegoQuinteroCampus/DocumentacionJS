7999b5fc8d79b0363d0a57871ba72653b379ef5c
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:58:03 -0500

    feat(docs):🚑 Strings.prototype.toUpperCase
    Borré el anterio y puse el concepto de string .toUpperCase ademas de agregarle ejmplos breves, este hace que el contenido s eponga en mayuscula

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 /* STRINGS */
 
-/* String.prototype.toLowerCase()
+/* String.prototype.toUpperCase()
 
-El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada. toLowerCase no afecta al valor de la cadena en sí misma.
- */
-
-let text = "Hello World!";
-let result = text.toLowerCase();
-
-document.getElementById("demo").innerHTML = result;
-//hello world!//
+El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada. */
+console.log( "alphabet".toUpperCase() ); // "ALPHABET"
