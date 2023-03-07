40dc41a6067e19247007e24d7563d259b1dfc3f5
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-07 14:34:19 -0500

    feat(docs):🚑 Strings.prototype.repeat
    Borré el anterio y puse el concepto de string .repeat para luego ponerle ejemplos..

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 /* STRINGS */
 
-/* String.prototype.matchAll()
-El método matchAll() retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, incluyendo grupos de captura. */
-
-const regexp = /t(e)(st(\d?))/g;
-const str = 'test1test2';
-
-const array = [...str.matchAll(regexp)];
-
-console.log(array[0]);
-// Expected output: Array ["test1", "e", "st1", "1"]
-
-console.log(array[1]);
-// Expected output: Array ["test2", "e", "st2", "2"]
+// Método repeat() // String.prototype.repeat()
+/* El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de
+copias de la cadena en la cual fue llamada, concatenados. */
+let text = "Hello world!";
+let result = text.repeat(2); 
+/* Hello world!Hello world! */
