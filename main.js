69751b734c303f90174373636403bf67cd1e5eb9
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-09 08:36:46 -0500

    feat: :ambulance: Operadores de Comparación
    Se le añadio nuevo cotenido eliminando el anterior, el nuevo contenido trata de los operadores por comparacion

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
 //Documentacion operadores//
 
-//Operadores Logicos//
-//se utilizan normalmente con valores booleanos (lógicos); cuando lo son, devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no booleanos, pueden devolver un valor no booleano//
+/* Operadores de Comparación */
+
+//Igual (==)//
+//Devuelve true si los operandos son iguales.
+3 == var1
+"3" == var1
+3 == '3'
 
 
-//AND Lógico (&&)//
-//Devuelve expr1 si se puede convertir a false; de lo contrario, devuelve expr2. Por lo tanto, cuando se usa con valores booleanos, && devuelve true si ambos operandos son true; de lo contrario, devuelve false.//
-var a3 = false && true;     // f && t devuelve false
-var a4 = false && (3 == 4); // f && f devuelve false
-var a5 = 'Cat' && 'Dog';    // t && t devuelve Dog
+//No es igual (!=)//	
+//Devuelve true si los operandos no son iguales.//	
+var1 != 4
+var2 != "3"
 
-//OR lógico (||)//
-//Devuelve expr1 si se puede convertir a true; de lo contrario, devuelve expr2. Por lo tanto, cuando se usa con valores booleanos, || devuelve true si alguno de los operandos es true; si ambos son falsos, devuelve false.//
-var o3 =  true || false;    // t || f devuelve true
-var o4 = false || (3 == 4); // f || f devuelve false
-var o5 = 'Cat' || 'Dog';    // t || t devuelve Cat
 
-//NOT lógico (!)//
-//Devuelve false si su único operando se puede convertir a true; de lo contrario, devuelve true.//
-var n1 = !true;  // !t devuelve false
-var n2 = !false; // !f devuelve true
-var n3 = !'Cat'; // !t devuelve false
+//Estrictamente igual (===)//
+//Devuelve true si los operandos son iguales y del mismo tipo. Consulta también Object.is y similitud en JS.//
+3 === var1
 
-//Exclusión lógica ^ (Xor).//
-//Establece una exclusión lógica de dos expresiones, es decir, que el resultado se dará evaluando una expresión u otra, y dará True si solamente una de ellas es True, lo que implica la exclusión de la otra.//
-El hierro es un metal = True.
-El hierro es duro = True.
-El hierro es un metal ^ El hierro es duro = False.
+
+//Desigualdad estricta (!==)//	
+//Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.//	
+var1 !== "3"
+3 !== '3'
+
+
+//Mayor que (>)//	
+//Devuelve true si el operando izquierdo es mayor que el operando derecho.//	
+var2 > var1 
+"12" > 2
+
+
+//Mayor o igual que (>=)//	
+//Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.//	
+var2 >= var1 
+var1 >= 3
+
+
+//Menor que (<)//	
+//Devuelve true si el operando izquierdo es menor que el operando derecho.//
+var1 < var2
+"2" < 12
+
+
+//Menor o igual (<=)//	
+//Devuelve true si el operando izquierdo es menor o igual que el operando derecho.//
+var1 <= var2 
+var2 <= 5
