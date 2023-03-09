eb7ca5f9fb709b537dce4e37984c38a36f2d790d
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-09 08:58:56 -0500

    feat: :ambulance: Math.random
    Se le agregó la funcion random el cual es la eleccion de numero dentro de un conjunto en aleatorio

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //Documentacion operadores//
 
-/* Math.min()
-
-La función Math.min() devuelve el menor de cero o más números, en otras palabras hace la selecion del numero mas pequeño
-Si no se proporcionan argumentos, el resultado es Infinity.
-
-Si al menos uno de los argumentos no puede ser convertido a número, el resultado es NaN. */
-
-console.log(Math.min(2, 3, 1));
-// Expected output: 1
-
-console.log(Math.min(-2, -3, -1));
-// Expected output: -3
-
-const array1 = [2, 3, 1];
-
-console.log(Math.min(...array1));
-// Expected output: 1
+/* Math.random()
+La función Math.random() devuelve un número de coma flotante pseudo-aleatorio, comprendido en el rango de 0 a menor que 1 (es decir, incluido el 0 pero no el 1), con una distribución aproximadamente uniforme en este rango. Dicho rango puede ser ampliado hasta los valores deseados. La implementación selecciona la semilla inicial del algoritmo de generación de números aleatorios, no pudiendo ser esta elegida o restablecida por el usuario. 
+(Elije un numero aleatorio)
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(3));
+  // Expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // Expected output: 0
+  
+  console.log(Math.random());
+  // Expected output: a number from 0 to <1
+  
