3829d0c5c90a30ca188096b70d338418f65a0489
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-09 08:30:24 -0500

    feat: :ambulance: Diferente tipos de operadores aritmeticos
    Iniciamos borrando el anterior contenido e iniciando nuevo contenido de operadores en js, en primera instancia operadores aritmeticos

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-/* STRINGS */
+//Documentacion operadores//
 
-/* String.raw()
+//Operadores aritméticos
+//Suma (+): Se suman dos numero o en lugar de esta se suman las cadenas en este caso produciendo una concatenacion Si alguno de los elementos a concatenar no es una cadena de texto, queda convertido a cadena: "AB" + 123 = "AB123"//
+3 + 2 = 5
+"A" + "B" = "AB"
+x= 5+8
 
-String. raw() es la unica función de etiqueta incorporada en las plantillas string; trabaja igual que la función de la plantilla por defecto y ejecuta la concatenación. Incluso puedes reimplementarlo con código normal de JavaScript
-El método estatico String.raw() es una función de plantilla de literales (en-US), similar al prefijo r en Python o al prefijo @ en C# para strings literales (con ciertas diferencias: ver la explicación en este problema). Se utiliza para obtener un string crudo a partir de plantillas de string (es decir, el original, texto no interpretado). */
+//
 
-String.raw`Hi\n${2+3}!`;
-//el carácter después de 'Hi'
-// no es un carácter de nueva línea,
-// '\' y 'n' son dos caracteres.
+//Resta (-)//
+//Halla la diferencia entre dos números. le quita la cantidad de numeros al otro//
+3-2 = 1
+(-1)-(-2) = 1
+
+//Negativo  (-)//
+//Indica el signo negativo de un número o una expresión en otras palabras indica que el numero esta por debajo de 0//
+
+-5
+-45
+
+//División (/)//
+//Divide dos números, obteniendo un cociente de coma flotante: 5 / 2 = 2.5//
+
+56/3
+
+//Módulo aritmético %//
+
+//Divide dos números, obteniendo un resto entero o sobrante//
+5 % 2 = 1
+
+//Multiplicación (*)//
+//multiplica los numeros pueden se diferente cantidad//
+56*3= 168
+
+
+
+//Incrementos y decrementos (++   --)//
+//Incrementa o decrementa el valor de una variable numérica en una unidad. No puede aplicarse a un literal. Pueden darse dos casos:
+
+var A, B; B = 2; 
+
+A =  ++B  
+A =  B++  
+
+//En el primer caso,  A = 3   Es decir, que el incremento de B ha sido el correcto, pero no así el segundo, en el que  A = 2. Esto se debe a que, en el primer caso, el incremento se efectúa antes de que la expresión sea evaluada, y en el segundo, se incrementa después de la evaluación según indica el orden de precedencia de los operadores (ver tabla al final de esta página). De igual forma, para decrementos:
+var A, B; B = 2; 
+
+A =  --B  
+A =  B--  
+
+//En el primer caso resulta:   A = 1   y en el segundo:   A = 2//
+
+
+
+
