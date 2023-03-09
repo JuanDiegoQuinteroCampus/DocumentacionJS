33c1a103a59347b068b003b331339712be9904ca
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-09 09:20:12 -0500

    feat: :ambulance: Math.trunc
    Se le agregó concepto y ejemplos acerca de la funcion math trunc

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 //Documentacion operadores//
 
-/* Math.round()
-La función Math.round()retorna el valor de un número redondeado al entero más cercano.
-Si la porción fraccionaría del número es 0.5 o mayor, el argumento es redondeado al siguiente número entero superior. Si la porción de la fracción del número es menor a 0.5, el argumento es redondeado al siguiente número entero inferior.
+/* Math.trunc()
 
-Debido a que round() es un método estático de Math, siempre se debe utilizar como Math.round(), en vez de un método del objeto Math que ha creado. (Math no es un constructor)
+La función Math.trunc() devuelve la parte entera de un numero removiendo cualquier dígito decimal (dígitos situados después de la coma).
 
-en otras palabras hace una aproximacion a un numero entero dependiendo del numero decimal
- */
-// Retorna el valor 20
-x = Math.round(20.49);
+en otras palabras elimina la parte decimal */
 
-// Retorna el valor 21
-x = Math.round(20.5);
-
-// Retorna el valor -20
-x = Math.round(-20.5);
-
-// Retorna el valor -21
-x = Math.round(-20.51);
+Math.trunc(13.37);    // 13
+Math.trunc(42.84);    // 42
+Math.trunc(0.123);    //  0
+Math.trunc(-0.123);   // -0
+Math.trunc('-1.123'); // -1
+Math.trunc(NaN);      // NaN
+Math.trunc('foo');    // NaN
+Math.trunc();         // NaN
