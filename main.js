4ec0d46d6989ae0820cc9ba8a5ac2715d1309f34
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-09 08:48:11 -0500

    feat: :ambulance: Math.PI
    Se le añadio una documentacion de math en especifico math.pi

--- main.js
+++ main.js
@@ -1,50 +1,17 @@
 //Documentacion operadores//
 
-/* Operadores de Comparación */
+/* Math.PI */
+/* 
+La propiedad Math.PI representa la relacion entre la longitud de la circunferencia de un circulo y su diametro, la cual es aproximadamente 3.14159.
+Debido a que PI es una propiedad estatica del modulo Math, siempre debes de utilizarla como Math.PI, en lugar de una propiedad de un objeto Math que tu hayas creado ( Math no es un constructor ). */
 
-//Igual (==)//
-//Devuelve true si los operandos son iguales.
-3 == var1
-"3" == var1
-3 == '3'
-
-
-//No es igual (!=)//	
-//Devuelve true si los operandos no son iguales.//	
-var1 != 4
-var2 != "3"
-
-
-//Estrictamente igual (===)//
-//Devuelve true si los operandos son iguales y del mismo tipo. Consulta también Object.is y similitud en JS.//
-3 === var1
-
-
-//Desigualdad estricta (!==)//	
-//Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.//	
-var1 !== "3"
-3 !== '3'
-
-
-//Mayor que (>)//	
-//Devuelve true si el operando izquierdo es mayor que el operando derecho.//	
-var2 > var1 
-"12" > 2
-
-
-//Mayor o igual que (>=)//	
-//Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.//	
-var2 >= var1 
-var1 >= 3
-
-
-//Menor que (<)//	
-//Devuelve true si el operando izquierdo es menor que el operando derecho.//
-var1 < var2
-"2" < 12
-
-
-//Menor o igual (<=)//	
-//Devuelve true si el operando izquierdo es menor o igual que el operando derecho.//
-var1 <= var2 
-var2 <= 5
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+  }
+  
+  console.log(Math.PI);
+  // Expected output: 3.141592653589793
+  
+  console.log(calculateCircumference(10));
+  // Expected output: 62.83185307179586
+  
