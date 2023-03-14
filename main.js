f223194ecbebb9af3fb6a3263b26c1cc1c1952ad
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 09:04:20 -0500

    feat: :ambulance: Funciones por expresión
    Se implemento un contenido que explique que son y para que sirven las funciones por expresion

--- main.js
+++ main.js
@@ -3,14 +3,24 @@
 /* Tipos de funciones */
 
 
-/* Funciones por declaración
-Esta forma permite declarar una función que existirá a lo largo de todo el código:
- */function saludar() {
-  return "Hola";
-}
+/* Funciones por expresión
 
-saludar(); // 'Hola'
-typeof saludar; // 'function'
-/* 
-De hecho, podríamos ejecutar la función saludar() incluso antes de haberla creado y funcionaría correctamente,
- ya que Javascript primero busca las declaraciones de funciones y luego procesa el resto del código. */
+en Javascript es muy habitual encontrarse códigos donde los programadores «guardan funciones» dentro de 
+variables, para posteriormente «ejecutar dichas variables». Se trata de un enfoque diferente, creación de 
+funciones por expresión, que fundamentalmente, hacen lo mismo con algunas diferencias:
+ */
+// El segundo "saludar" (nombre de la función) se suele omitir: es redundante
+const saludo = function saludar() {
+  return "Hola";
+};
+
+saludo(); // 'Hola'
+
+/* Estamos creando una función en el interior de una variable, lo que nos permitirá posteriormente ejecutar 
+la variable (como si fuera una función). Observa que el nombre de la función (en este ejemplo: saludar) 
+pasa a ser inútil, ya que si intentamos ejecutar saludar() nos dirá que no existe y si intentamos ejecutar 
+saludo() funciona correctamente.
+
+¿Qué ha pasado? Ahora el nombre de la función pasa a ser el nombre de la variable, mientras 
+que el nombre de la función desaparece y se omite, dando paso a lo que se llaman las funciones anónimas 
+(o funciones lambda). */
