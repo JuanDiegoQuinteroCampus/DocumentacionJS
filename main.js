7a3b9ec3a32d94f2d5b70c50392e298168ac540f
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 08:53:12 -0500

    feat: :ambulance: ¿Que son las funciones?
    Iniciamos con un tema dando explicacion a que son las funciones y para que se utilizan en Javascript

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-/* Number.prototype.toFixed()
+/* FUNCIONES */
 
-El método toFixed() formatea un número usando notación de punto fijo.
- */
-function financial(x) {
-    return Number.parseFloat(x).toFixed(2);
-  }
-  
-  console.log(financial(123.456));
-  // Expected output: "123.46"
-  
-  console.log(financial(0.004));
-  // Expected output: "0.00"
-  
-  console.log(financial('1.23e+5'));
-  // Expected output: "123000.00"
+/*        ¿Qué es una función?
+Las funciones nos permiten agrupar líneas de código en tareas con un nombre, para que, posteriormente, podamos hacer referencia a ese nombre para realizar todo lo que se agrupe en dicha tarea. Para usar funciones hay que hacer 2 cosas:
 
-  var numObj = 12345.6789;
+    Declarar la función: Preparar la función, darle un nombre y decirle las tareas que realizará.
+    Ejecutar la función: «Llamar» a la función para que realice las tareas de su contenido.
 
-/* ------------------Mas entendible-------------------- */
+    Creación de funciones
 
-numObj.toFixed();       // Returns '12346': note rounding, no fractional part
-numObj.toFixed(1);      // Returns '12345.7': note rounding
-numObj.toFixed(6);      // Returns '12345.678900': note added zeros
-(1.23e+20).toFixed(2);  // Returns '123000000000000000000.00'
+Hay varias formas de crear funciones en Javascript: por declaración (la más usada por principiantes), por expresión (la más habitual en programadores con experiencia) o mediante constructor de objeto (no recomendada):
+
+Constructor                                     	Descripción
+function nombre(p1, p2...) { } =	          Crea una función mediante declaración.
+var nombre = function(p1, p2...) { } =	    Crea una función mediante expresión.
+new Function(p1, p2..., code); =	          Crea una función mediante un constructor de objeto. */
