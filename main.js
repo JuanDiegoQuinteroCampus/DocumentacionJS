a7e351fec43ecbd9ea3899cac70141e1febe463d
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 12:54:55 -0500

    feat: :ambulance: Clausuras
    Se le agrego una clausura para cerar variables en su propio ambito

--- main.js
+++ main.js
@@ -2,28 +2,25 @@
 
 /* Tipos de funciones */
 
-/* 
-Funciones autoejecutables
+/* Clausuras
 
-En Javascript es muy sencillo crear funciones autoejecutables. Básicamente, sólo tenemos que envolver entre paréntesis la función anónima
- en cuestión (no necesitamos que tenga nombre, puesto que no la vamos a guardar) y luego, ejecutarla: */
-
-// Función autoejecutable
-(function () {
-  console.log("Hola!!");
+A grandes rasgos, en Javascript, una clausura o cierre se define como una función que «encierra» variables en su propio ámbito 
+(y que continúan existiendo aún habiendo terminado la función). Por ejemplo, veamos el siguiente ejemplo:
+ */
+// Clausura: Función incr()
+const incr = (function () {
+  let num = 0;
+  return function () {
+    num++;
+    return num;
+  };
 })();
 
-// Función autoejecutable con parámetros
-(function (name) {
-  console.log(`¡Hola, ${name}!`);
-})("Manz");
+typeof incr; // 'function'
+incr(); // 1
+incr(); // 2
+incr(); // 3
+/* La «magia» de las clausuras es que en el interior de la función autoejecutable estamos creando una variable num que se guardará en el ámbito de dicha función, por lo tanto existirá con el valor declarado: 0.
 
-/* Ten en cuenta, que si la función autoejecutable devuelve algún valor con return, a diferencia de las funciones por expresión, 
-en este caso lo que se almacena en la variable es el valor que devuelve la función autoejecutada:
- */
-const f = (function (name) {
-  return `¡Hola, ${name}!`;
-})("Manz");
-
-f; // '¡Hola, Manz!`
-typeof f; // 'string'
+Por lo tanto, en la variable incr tenemos una función por expresión que además conoce el valor de una variable num, que sólo existe dentro de incr. Si nos fijamos en la función que devolvemos, lo que hace es incrementar el valor de num y devolverlo. Como la variable incr es una clausura y mantiene la variable en su propio ámbito, veremos que a medida que ejecutamos incr(),
+ los valores de num (que estamos devolviendo) conservan su valor y se van incrementando. */
