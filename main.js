c0fec022345dbe7e28fb112068bf899f94930889
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 12:57:37 -0500

    feat: :ambulance: Arrow Functions
    Se le añadio contenido sobre arrow function que trata de avreviar las funciones en si

--- main.js
+++ main.js
@@ -2,25 +2,31 @@
 
 /* Tipos de funciones */
 
-/* Clausuras
 
-A grandes rasgos, en Javascript, una clausura o cierre se define como una función que «encierra» variables en su propio ámbito 
-(y que continúan existiendo aún habiendo terminado la función). Por ejemplo, veamos el siguiente ejemplo:
+/* Arrow functions
+
+Las Arrow functions, funciones flecha o «fat arrow» son una forma corta de escribir funciones que aparece en Javascript a partir de ECMAScript 6.
+ Básicamente, se trata de reemplazar eliminar la palabra function y añadir => antes de abrir las llaves:
  */
-// Clausura: Función incr()
-const incr = (function () {
-  let num = 0;
-  return function () {
-    num++;
-    return num;
-  };
-})();
+const func = function () {
+  return "Función tradicional.";
+};
 
-typeof incr; // 'function'
-incr(); // 1
-incr(); // 2
-incr(); // 3
-/* La «magia» de las clausuras es que en el interior de la función autoejecutable estamos creando una variable num que se guardará en el ámbito de dicha función, por lo tanto existirá con el valor declarado: 0.
+const func = () => {
+  return "Función flecha.";
+};
 
-Por lo tanto, en la variable incr tenemos una función por expresión que además conoce el valor de una variable num, que sólo existe dentro de incr. Si nos fijamos en la función que devolvemos, lo que hace es incrementar el valor de num y devolverlo. Como la variable incr es una clausura y mantiene la variable en su propio ámbito, veremos que a medida que ejecutamos incr(),
- los valores de num (que estamos devolviendo) conservan su valor y se van incrementando. */
+/* Sin embargo, las funciones flechas tienen algunas ventajas a la hora de simplificar código bastante interesantes:
+
+    Si el cuerpo de la función sólo tiene una línea, podemos omitir las llaves ({}).
+    Además, en ese caso, automáticamente se hace un return de esa única línea, por lo que podemos omitir también el return.
+    En el caso de que la función no tenga parámetros, se indica como en el ejemplo anterior: () =>.
+    En el caso de que la función tenga un solo parámetro, se puede indicar simplemente el nombre del mismo: e =>.
+    En el caso de que la función tenga 2 ó más parámetros, se indican entre paréntesis: (a, b) =>.
+    Si queremos devolver un objeto, que coincide con la sintaxis de las llaves, se puede englobar con paréntesis: ({name: 'Manz'}).
+
+Por lo tanto, el ejemplo anterior se puede simplificar aún más: */
+
+const func = () => "Función flecha."; // 0 parámetros: Devuelve "Función flecha"
+const func = (e) => e + 1; // 1 parámetro: Devuelve el valor de e + 1
+const func = (a, b) => a + b; // 2 parámetros: Devuelve el valor de a + b
