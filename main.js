38254f4f5ef6f42c4c436070b0f14e55710cb988
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 09:00:40 -0500

    feat: :ambulance: Funciones  por declaracion
    Se le documento para que es una funcion declarada a lo largo del codigo

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /* FUNCIONES */
 
-/*        ¿Qué es una función?
-Las funciones nos permiten agrupar líneas de código en tareas con un nombre, para que, posteriormente, podamos hacer referencia a ese nombre para realizar todo lo que se agrupe en dicha tarea. Para usar funciones hay que hacer 2 cosas:
+/* Tipos de funciones */
 
-    Declarar la función: Preparar la función, darle un nombre y decirle las tareas que realizará.
-    Ejecutar la función: «Llamar» a la función para que realice las tareas de su contenido.
 
-    Creación de funciones
+/* Funciones por declaración
+Esta forma permite declarar una función que existirá a lo largo de todo el código:
+ */function saludar() {
+  return "Hola";
+}
 
-Hay varias formas de crear funciones en Javascript: por declaración (la más usada por principiantes), por expresión (la más habitual en programadores con experiencia) o mediante constructor de objeto (no recomendada):
-
-Constructor                                     	Descripción
-function nombre(p1, p2...) { } =	          Crea una función mediante declaración.
-var nombre = function(p1, p2...) { } =	    Crea una función mediante expresión.
-new Function(p1, p2..., code); =	          Crea una función mediante un constructor de objeto. */
+saludar(); // 'Hola'
+typeof saludar; // 'function'
+/* 
+De hecho, podríamos ejecutar la función saludar() incluso antes de haberla creado y funcionaría correctamente,
+ ya que Javascript primero busca las declaraciones de funciones y luego procesa el resto del código. */
