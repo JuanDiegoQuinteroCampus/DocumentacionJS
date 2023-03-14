092d15eca31110779fea6673bec8a1e868ead579
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 09:19:21 -0500

    feat: :ambulance: Funciones anonimas
    Se le inserto de contenido un concepto con ejemplo acerca de funciones anonimas

--- main.js
+++ main.js
@@ -3,10 +3,17 @@
 /* Tipos de funciones */
 
 
-/* Funciones como objetos
+/* Funciones anónimas
 
-Es un enfoque que no se suele utilizar en producción. Simplemente es interesante saberlo para darse cuenta que en Javascript todo pueden ser objetos:
- */
-const saludar = new Function("return 'Hola';");
+Las funciones anónimas o funciones lambda son un tipo de funciones que se declaran sin nombre de función 
+y se alojan en el interior de una variable y haciendo referencia a ella cada vez que queramos utilizarla:
+ */// Función anónima "saludo"
+const saludo = function () {
+  return "Hola";
+};
 
-saludar(); // 'Hola'
+saludo; // ƒ () { return 'Hola'; }
+saludo(); // 'Hola'
+/* la última línea del ejemplo anterior, estamos ejecutando la variable, es decir, ejecutando la 
+función que contiene la variable. Sin embargo, en la línea anterior hacemos referencia a la variable 
+(sin ejecutarla, no hay paréntesis) y nos devuelve la función en sí. */
