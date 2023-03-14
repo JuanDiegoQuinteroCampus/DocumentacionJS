7b9f56a61889e88c2a3eb3fd8089101df8be1b41
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-14 09:07:53 -0500

    feat: :ambulance: Funciones como objetos
    Se explica la funcion como objeto, y se le quitó el contenido anterior

--- main.js
+++ main.js
@@ -3,24 +3,10 @@
 /* Tipos de funciones */
 
 
-/* Funciones por expresión
+/* Funciones como objetos
 
-en Javascript es muy habitual encontrarse códigos donde los programadores «guardan funciones» dentro de 
-variables, para posteriormente «ejecutar dichas variables». Se trata de un enfoque diferente, creación de 
-funciones por expresión, que fundamentalmente, hacen lo mismo con algunas diferencias:
+Es un enfoque que no se suele utilizar en producción. Simplemente es interesante saberlo para darse cuenta que en Javascript todo pueden ser objetos:
  */
-// El segundo "saludar" (nombre de la función) se suele omitir: es redundante
-const saludo = function saludar() {
-  return "Hola";
-};
+const saludar = new Function("return 'Hola';");
 
-saludo(); // 'Hola'
-
-/* Estamos creando una función en el interior de una variable, lo que nos permitirá posteriormente ejecutar 
-la variable (como si fuera una función). Observa que el nombre de la función (en este ejemplo: saludar) 
-pasa a ser inútil, ya que si intentamos ejecutar saludar() nos dirá que no existe y si intentamos ejecutar 
-saludo() funciona correctamente.
-
-¿Qué ha pasado? Ahora el nombre de la función pasa a ser el nombre de la variable, mientras 
-que el nombre de la función desaparece y se omite, dando paso a lo que se llaman las funciones anónimas 
-(o funciones lambda). */
+saludar(); // 'Hola'
