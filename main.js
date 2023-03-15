15b691812d8e8b8e3010c01fcbf6eb830abf2bc7
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-15 06:37:02 -0500

    feat: :ambulance: FormData
    Se le hizo un cambio de contenido agregando formdata que trata de almacenamiento de datos

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-/* Object.fromEntries()
-
-El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto. */
-
-const entries = new Map([
-  ['foo', 'bar'],
-  ['baz', 42]
-]);
-
-const obj = Object.fromEntries(entries);
-
-console.log(obj);
-// Expected output: Object { foo: "bar", baz: 42 }
+/* FormData()
 
 
+Los objetos FormData le permiten compilar un conjunto de pares clave/valor para enviar mediante XMLHttpRequest. Están destinados principalmente para el envío de 
+los datos del formulario, pero se pueden utilizar de forma independiente con el fin de transmitir los datos tecleados. Los datos transmitidos estarán en el mismo 
+formato que usa el método submit() del formulario para enviar los datos si el tipo de codificación del formulario se establece en "multipart/form-data".
+ */
+var formData = new FormData();
 
-/* En otras palabras Object.fromEntries sirve para cambiar un objeto o cualquier contenido por uno diferente */
+formData.append("username", "Groucho");
+formData.append("accountnum", 123456); // number 123456 is immediately converted to string "123456"
 
-const object1 = { a: 1, b: 2, c: 3 };
+/* Recuperando un objeto FormData de un formulario HTML
 
-const object2 = Object.fromEntries(
-  Object.entries(object1)
-  .map(([ key, val ]) => [ key, val * 2 ])
-);
+Para construir un objeto FormData que contenga los datos de un <form> existente, especifique ese elemento form cuando cree el objeto FormData:
+ */
+var formData = new FormData(someFormElement);
 
-console.log(object2);
-// { a: 2, b: 4, c: 6 }
+/* Por ejemplo: */
+
+var formElement = document.getElementById("myFormElement");
+var request = new XMLHttpRequest();
+request.open("POST", "submitform.php");
+request.send(new FormData(formElement));
+
+/* También puede añadir datos adicionales al objeto FormData antes de enviarlo. Así:
+ */
+var formElement = document.getElementById("myFormElement");
+formData = new FormData(formElement);
+formData.append("serialnumber", serialNumber++);
+request.send(formData);
