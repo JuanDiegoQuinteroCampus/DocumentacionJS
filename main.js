c03296745796b1c74dbea00280c0f4418e25c08a
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-15 06:10:32 -0500

    feat: :ambulance: Object.fromEntries()
    Se le añadio como contenido una explicación de Object.fromEntries(), que trata de un cambio de contenido

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-/* FUNCIONES */
+/* Object.fromEntries()
 
-/* Tipos de funciones */
+El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto. */
+
+const entries = new Map([
+  ['foo', 'bar'],
+  ['baz', 42]
+]);
+
+const obj = Object.fromEntries(entries);
+
+console.log(obj);
+// Expected output: Object { foo: "bar", baz: 42 }
 
 
-/* Arrow functions
 
-Las Arrow functions, funciones flecha o «fat arrow» son una forma corta de escribir funciones que aparece en Javascript a partir de ECMAScript 6.
- Básicamente, se trata de reemplazar eliminar la palabra function y añadir => antes de abrir las llaves:
- */
-const func = function () {
-  return "Función tradicional.";
-};
+/* En otras palabras Object.fromEntries sirve para cambiar un objeto o cualquier contenido por uno diferente */
 
-const func = () => {
-  return "Función flecha.";
-};
+const object1 = { a: 1, b: 2, c: 3 };
 
-/* Sin embargo, las funciones flechas tienen algunas ventajas a la hora de simplificar código bastante interesantes:
+const object2 = Object.fromEntries(
+  Object.entries(object1)
+  .map(([ key, val ]) => [ key, val * 2 ])
+);
 
-    Si el cuerpo de la función sólo tiene una línea, podemos omitir las llaves ({}).
-    Además, en ese caso, automáticamente se hace un return de esa única línea, por lo que podemos omitir también el return.
-    En el caso de que la función no tenga parámetros, se indica como en el ejemplo anterior: () =>.
-    En el caso de que la función tenga un solo parámetro, se puede indicar simplemente el nombre del mismo: e =>.
-    En el caso de que la función tenga 2 ó más parámetros, se indican entre paréntesis: (a, b) =>.
-    Si queremos devolver un objeto, que coincide con la sintaxis de las llaves, se puede englobar con paréntesis: ({name: 'Manz'}).
-
-Por lo tanto, el ejemplo anterior se puede simplificar aún más: */
-
-const func = () => "Función flecha."; // 0 parámetros: Devuelve "Función flecha"
-const func = (e) => e + 1; // 1 parámetro: Devuelve el valor de e + 1
-const func = (a, b) => a + b; // 2 parámetros: Devuelve el valor de a + b
+console.log(object2);
+// { a: 2, b: 4, c: 6 }
